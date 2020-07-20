f1ab18ca83f4106ef0347d11c6e805dc8f75fc15
nima.siboni <nima.siboni@gmail.com>
2020-07-20 15:48:46 +0200

    feature engineering started:
    [Added] feature_engineering_utils.py : file with some feature engineering functions. so far the store id is sorted for Sale
    
    [Added] feature_engineering.py : all the feature engineering happens here.

---
+++ feature_engineering.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+from clean import clean
+from feature_engineering_utils import creating_conversion_table
+from feature_engineering_utils import creating_df_with_sorted_id
+
+# 1 creating a new column with store id's which are realted to Sales
+
+# 1.1
+conversion = creating_conversion_table()
+# 1.2
+df_v_1 = creating_df_with_sorted_id(conversion)
