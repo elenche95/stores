5bce30240b200360330587e9c0a4748470ac06d5
Gleb Sidorov Moiseenko <55404877+gsidorov@users.noreply.github.com>
2020-07-22 16:16:09 +0200

    Update express.py

--- express.py
+++ express.py
@@ -1,9 +1,14 @@
 from clean2 import clean
 import pandas as pd
 import numpy as np
+
+print('Cleaning and loading test set ...')
 df = clean('./data/test.csv')
 
+print('Model loaded, applying transformations')
 cat_variables = ['StateHoliday_new', 'StoreType', 'Assortment', 'PromoInterval']
+
+print(f'Number of rows in test.csv: {len(df)}')
 
 df_final = pd.get_dummies(df, columns = cat_variables, drop_first=True)
 
@@ -13,8 +18,10 @@ Y_test = df_final.Sales
 
 from joblib import dump, load
 
+print('Loading 8GB of Random Forest Regressor model...')
 dtree = load('./dtree_final.joblib')
 
+print('Predicting Sales for the test set')
 predictions = dtree.predict(X_test)
 
 def metric(preds, actuals):
@@ -24,4 +31,5 @@ def metric(preds, actuals):
     return 100 * np.linalg.norm((actuals - preds) / actuals) / np.sqrt(preds.shape[0])
 
 
-print(f'RSMPE: {round(metric(np.array(predictions), np.array(Y_test)), 2)}')   
+print(f'Evaluation completed for {len(df)} rows')
+print(f'RSMPE: {round(metric(np.array(predictions), np.array(Y_test)), 2)}%')   
