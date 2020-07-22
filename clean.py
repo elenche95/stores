3138cf46b6b473fef0a098cfe3102d3ebf8d5788
elenche95 <elena.stein95@gmail.com>
2020-07-22 09:48:09 +0200

    modify clean to edit test set also

--- clean.py
+++ clean.py
@@ -25,7 +25,7 @@ def customers_nan(sales, customers):
 
 # Cleaning training dataset
 
-def train_clean(path):
+def train_clean(path, test):
     
     # Setting index and time auxiliary columns
     train = pd.read_csv(path, parse_dates=True, index_col='Date')
@@ -52,8 +52,9 @@ def train_clean(path):
     train.drop(axis=1, labels='DayOfWeek', inplace=True)
     
     #Substitute missing values in customers by the mean of Sales and Customers
-    
-    train['Customers'] = train.apply(lambda x: customers_nan(x['Sales'], x['Customers']), axis=1)
+    #does not work for test data  
+    if test==False:
+        train['Sales_per_Customer'] = train.apply(lambda x: customers_nan(x['Sales'], x['Customers']), axis=1)
     
     # Set Open Column to 1 OR DROP IT
     
@@ -68,15 +69,20 @@ def train_clean(path):
     train.drop(axis=1, labels= ['StateHoliday'], inplace=True)
     
     train['SchoolHoliday'] = train['SchoolHoliday'].fillna(0)
-    
+
     return train
 
 
-def clean(path):
-    """ Add the path to your train data or your test data in order to apply the transformations to it"""
-    #This function takes the string argument for the path of the train.csv input str_path
+def clean(str_path, test=False):
+    """ This is a function to clean data,
+    parameters:
+    - str_path is a path of the data
+    - test=True if this is a test set
     
-    train = train_clean(path)
+    """
+    #This function takes the string argument for the path of the train.csv input str_path
+    path = Path(str_path)
+    train = train_clean(path, test)
     store = store_clean()
     
     merged = train.merge(store, on='Store', how='inner')
